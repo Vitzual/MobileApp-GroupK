ac9200c6fe10e025cb440cced52f624c97f8e684
Robert <rob.stewart.79@gmail.com>
2021-02-10 17:51:25 -0800

    Wrote the java code in activity main for button, button-button listener, and intent/start new activity.

--- app/src/main/java/group/k/MainActivity.java
+++ app/src/main/java/group/k/MainActivity.java
@@ -2,13 +2,31 @@ package group.k;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button robsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        robsButton = findViewById(R.id.button_robert);
+        robsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRobsActivity();
+            }
+        });
+    }
+
+    private void openRobsActivity() {
+        Intent intent = new Intent(this, robsPage.class );
+        startActivity(intent);
     }
 }
