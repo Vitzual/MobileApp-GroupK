3cee41369d3ffc2f16a357249e7d5be131c3dff5
Vitzual <65577955+Vitzual@users.noreply.github.com>
2021-02-10 23:49:00 -0800

    Added page class and activity xml

---
+++ app/src/main/java/group/k/BensPage.java
@@ -0,0 +1,13 @@
+package group.k;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BensPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ben);
+    }
+}
