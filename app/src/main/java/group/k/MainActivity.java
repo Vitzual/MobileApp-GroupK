f53718192810e1bacdbe0ee8c4c80b14b2afde96
chloeknispel <chloeknispel9@gmail.com>
2021-02-10 18:38:58 -0800

    Added my button to link to my page

--- app/src/main/java/group/k/MainActivity.java
+++ app/src/main/java/group/k/MainActivity.java
@@ -2,13 +2,32 @@ package group.k;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button=(Button)findViewById(R.id.button_chloe);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openchloeActivity();
+            }
+        });
+    }
+
+    public void openchloeActivity()
+    {
+        Intent intent=new Intent(this, chloeActivity.class);
+        startActivity(intent);
     }
 }
